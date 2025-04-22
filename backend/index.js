8c71a2cba73f45f7faf9e23893efc1e6c6c2f0c6
Prajjwal888 <chouhanprajjwal8@gmail.com>
2025-04-22 20:10:27 +0530

    add mongoose connection

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,13 @@
 import express from "express";
 import cors from "cors";
 import teacherRoute from "./routes/teacherRoutes.js";
+import dotenv from 'dotenv'
+dotenv.config();
+import connectDB from "./config/db.js";
 const app = express();
 app.use(cors());
 app.use(express.json());
+connectDB();
 const PORT = 5000;
 
 app.use(teacherRoute);
